cb4b74eeca7515c2ec42f14655e00aa09ab49901
Clemerson da Silva <francisco.pereira77510@alunos.ufersa.edu.br>
2024-11-12 11:19:05 -0300

    algumas modificacoes: controle de cache para casos em que produtos sao atualizados ou deletados para que os dados do cache sejam atualizados

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,79 +8,85 @@ use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
-{   
-    // correcao de bug que faz com que usuario submita formularios multiplas vezes
-    // cadastro de produto
-    public function create(Request $request){
-        $input = $request->validate(
-           [ 
-            'name' =>   'required|max:20',
-            'description' =>   'required|max:55',
-            'price' =>   'required',// é necessario correcao aqui para impedir que usuarios digitem valors com ',' separando as casas decimasi
-           ]
-        );
-
-        $input[ 'name']= strip_tags( $input[ 'name'])  ;
-        $input[ 'description']= strip_tags( $input[ 'description'])  ;
-        $input[ 'price']= strip_tags( $input[ 'price'])  ;
-
-        Product::create($input);
-        return redirect('/products')->with(['message' => 'roduto cadastrado com sucesso!']);
-      }
-
-      public function showSales(){
-        $products = get_products_data();
-        $clients = json_encode(DB::table('client')->paginate(10));
-        return view('crud_sales', compact('products', 'clients' ));
-}     
-
-      public function showDashboard(){
-        $products = $this->get_products_data();
-        $clients = json_encode(DB::table('client')->orderBy('name')->get());
-        $controller = new Sales();
-        $sales = json_encode($controller->get_sales_data()->forPage(1,10));
-        return view('dashboard', compact('products','sales', 'clients' ));
-      }
-
-     public function showProduct(Product $product){
-      $product = json_encode($product);
-      return view('edit_product', compact('product'));
-     }
-
-      public function edit(Product $product, Request $request){
-        $input = $request->validate(
-          [ 
-           'name' =>   'required|max:20',
-           'description' =>   'required|max:55',
-           'price' =>   'required',
-          ]
-       );
-
-       $product['name'] = $input['name'];
-       $product['description'] = $input['description'];
-       $product['price'] = $input['price'];
-
-       $product->update($input);
-       return redirect('/')->with(['message' =>  'Produto atualizado com sucesso!']);
-      }
-      
-      public function delete(Product $product){
-        $product->delete();
-        return redirect('/');
-      }
-      
-function get_products_data($id=false)
 {
-  if($id){
-    $products = Cache::remember('products-dashboard', 60*60, function($id){
-            return DB::table('products')->where('id', $id)->get();
-        });
-    }else{
-        $products = Cache::remember('products-dashboard', 60*60, function(){
-           return DB::table('products')->orderBy('name')->paginate(10);
-        });
+  // correcao de bug que faz com que usuario submita formularios multiplas vezes
+  // cadastro de produto
+  public function create(Request $request)
+  {
+    $input = $request->validate(
+      [
+        'name' =>   'required|max:20',
+        'description' =>   'required|max:55',
+        'price' =>   'required', // é necessario correcao aqui para impedir que usuarios digitem valors com ',' separando as casas decimasi
+      ]
+    );
+
+    $input['name'] = strip_tags($input['name']);
+    $input['description'] = strip_tags($input['description']);
+    $input['price'] = strip_tags($input['price']);
+
+    Product::create($input);
+    return redirect('/products')->with(['message' => 'roduto cadastrado com sucesso!']);
+  }
+
+  public function showSales()
+  {
+    $products = $this->get_products_data();
+    $clients = json_encode(DB::table('client')->paginate(10));
+    return view('crud_sales', compact('products', 'clients'));
+  }
+
+  public function showDashboard()
+  {
+    $products = $this->get_products_data();
+    $clients = json_encode(DB::table('client')->orderBy('name')->get());
+    $controller = new Sales();
+    $sales = json_encode($controller->get_sales_data()->forPage(1, 10));
+    return view('dashboard', compact('products', 'sales', 'clients'));
+  }
+
+  public function showProduct(Product $product)
+  {
+    $product = json_encode($product);
+    return view('edit_product', compact('product'));
+  }
+
+  public function edit( $id, Request $request)
+  {
+    $input = $request->validate(
+      [
+        'name' =>   'required|max:20',
+        'description' =>   'required|max:55',
+        'price' =>   'required',
+      ]
+    );
+
+    $productName = $input['name'];
+    $productDescription = $input['description'];
+    $productPrice = $input['price'];
+    DB::table('products')->where('id', $id)->update(['name'=> $productName, 'description' => $productDescription, 'price'=> $productPrice]);
+    Cache::forget('products-dashboard');    
+    return redirect('/')->with(['message' =>  'Produto atualizado com sucesso!']);
+  }
+// APLICAR SOFTDELTE NOS PRODUTOS
+  public function delete(Product $product)
+  {
+    $product->delete();
+    Cache::forget('products-dashboard');
+    return redirect('/');
+  }
+
+  function get_products_data($id = false)
+  {
+    if ($id) {
+      $products = Cache::remember('products-dashboard', 60 * 60, function ($id) {
+        return DB::table('products')->where('id', $id)->get();
+      });
+    } else {
+      $products = Cache::remember('products-dashboard', 60 * 60, function () {
+        return DB::table('products')->orderBy('name')->paginate(10);
+      });
     }
-    return $products;
-}
-      
+    return json_encode($products);
+  }
 }
